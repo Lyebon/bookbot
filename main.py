5dd7e60ef41306652c715589b98060733e08ac33
Lyebon <sebas25garcia@gmail.com>
2025-06-09 19:58:45 -0300

    finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import *
-
+import sys
 
 def get_book_text(relative_path):
     with open(relative_path) as f:
@@ -7,12 +7,17 @@ def get_book_text(relative_path):
         return content
 
 def main():
-    content = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
+    content = get_book_text(book)
     count = count_words(content)
     letters = count_letters(content)
     sort_letters = sort_on_char(letters)
     
-    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n")
+
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {book}...\n")
     print(f"----------- Word Count ----------\n    Found {count} total words\n--------- Character Count -------")
     for letter in sort_letters:
         print(f"{letter["char"]}: {letter["num"]}")
